488a68f6506b39ecef56d2a717b6214cf8616dbd
Arystar01 <aryanrastogi1921@gmail.com>
2024-12-24 13:19:54 +0530

    ...

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -4,24 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        console.log("Mongo URI:", process.env.MONGO_URI); // Log to ensure it's loaded correctly
+  try {
+    // console.log("Mongo URI:", process.env.MONGO_URI); 
 
-        if (!process.env.MONGO_URI) {
-            throw new Error("Mongo URI not defined");
-        }
-
-        // Removed the deprecated options
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, // This is still valid
-            useUnifiedTopology: true, // This is still valid
-        });
-
-        console.log("Connected to MongoDB 123 ");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error.message);
-        process.exit(1); // Exit process if connection fails
+    if (!process.env.MONGO_URI) {
+      throw new Error("Mongo URI not defined");
     }
+
+    await mongoose.connect(process.env.MONGO_URI); 
+
+    console.log("Connected to MongoDB 123 ");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); 
+  }
 };
 
 export default connectDB;
